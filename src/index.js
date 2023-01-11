0cd3a49cfd2d914c2b3f51f6dece4938a4dcccf5
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-01-11 20:40:52 +0530

    added add todo functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 // ReactDOM.render(
 // 	<React.StrictMode>
@@ -10,4 +12,4 @@ import App from './App';
 // 	document.getElementById('root')
 // );
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render( <Provider store={store}> <App /></Provider>);
